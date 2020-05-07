50a2fd6bf9d2b8ec0429ecf1ea6343fb3036f2a8
Lizzy <lizzy.brooks@gmail.com>
2020-05-07 11:24:09 -0700

    messed with perlin noise sketch

--- Game-Project-master/empty-example/sketch.js
+++ Game-Project-master/empty-example/sketch.js
@@ -1,10 +1,12 @@
 let noiseScale=0.02;
 
 function draw() {
+  createCanvas(400,400)
 background(0,200,230);
   for (let x=0; x < width; x++) {
-    let noiseVal = noise((mouseX+x)*noiseScale, mouseY*noiseScale);
+    let noiseVal = noise((x)*noiseScale/2, 80*noiseScale);
     stroke(235);
-    line(x, mouseY+noiseVal*80, x, height);
+    line(x, noiseVal*200+200, x, height);
+    
   }
 }

bef36f51583607f36f531bbd243214b6294806f1
Lizzy <lizzy.brooks@gmail.com>
2020-05-12 15:08:51 -0700

    made the snowscape move!

--- Game-Project-master/empty-example/sketch.js
+++ Game-Project-master/empty-example/sketch.js
@@ -1,12 +1,46 @@
-let noiseScale=0.02;
+let snowscape = [];
+
+function setup(){
+    createCanvas(800,400)
+}
+
 
 function draw() {
-  createCanvas(400,400)
-background(0,200,230);
-  for (let x=0; x < width; x++) {
-    let noiseVal = noise((x)*noiseScale/2, 80*noiseScale);
-    stroke(235);
-    line(x, noiseVal*200+200, x, height);
-    
+
+  background(0,200,230);
+
+  let drift = new Snow(width,frameCount);
+  snowscape.push(drift);
+
+  for (let i = 0; i<snowscape.length; i++){
+    snowscape[i].drawSnow();
+    snowscape[i].moveSnow();
   }
+
+  if (snowscape.length>width){
+    snowscape.shift();
+  }
+
+
+}
+
+class Snow{
+  constructor(x,startx){
+    this.x=x;
+    this.startx = startx;
+  }
+
+  drawSnow(){
+    let noiseScale=0.02;
+    let noiseVal = noise((this.startx)*noiseScale/2, 80*noiseScale);
+    stroke(235);
+    line(this.x, noiseVal*200+200, this.x, height);
+
+  }
+
+  moveSnow(){
+    this.x = this.x -1;
+
+  }
+
 }

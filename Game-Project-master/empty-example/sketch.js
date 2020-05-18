2f96f1c08bfa60ecf1e5a12f37f02fccdf355274
Lizzy <lizzy.brooks@gmail.com>
2020-05-18 10:21:24 -0700

    added snowscape detection

--- Game-Project-master/empty-example/sketch.js
+++ Game-Project-master/empty-example/sketch.js
@@ -1,7 +1,10 @@
 let snowscape = [];
+let me;
+let groundtest = [0,200,230,255];
 
 function setup(){
     createCanvas(800,400)
+    me = new Avatar(200,height/2);
 }
 
 
@@ -9,7 +12,8 @@ function draw() {
 
   background(0,200,230);
 
-  let drift = new Snow(width,frameCount);
+
+  let drift = new Snow(width,frameCount,200);
   snowscape.push(drift);
 
   for (let i = 0; i<snowscape.length; i++){
@@ -17,30 +21,78 @@ function draw() {
     snowscape[i].moveSnow();
   }
 
+
   if (snowscape.length>width){
     snowscape.shift();
   }
+if (frameCount<650){
+  textAlign(CENTER)
+  noStroke();
+  text("Welcome to this game! Move the snowboarder to jump over the snow. Your game will begin in a few seconds", width/2,height/2)
+}
+if(frameCount>650){
+  me.drawMe();
+  me.moveMe();
+}
+
+
+  groundtest = get(me.x,me.y+21);
+
+
+
 
 
 }
 
 class Snow{
-  constructor(x,startx){
+  constructor(x,startx,top_of_the_snow){
     this.x=x;
     this.startx = startx;
+    this.top_of_the_snow = top_of_the_snow;
   }
 
   drawSnow(){
     let noiseScale=0.02;
     let noiseVal = noise((this.startx)*noiseScale/2, 80*noiseScale);
     stroke(235);
-    line(this.x, noiseVal*200+200, this.x, height);
+    this.top_of_the_snow = noiseVal*200+200
+    line(this.x, this.top_of_the_snow, this.x, height);
 
   }
 
   moveSnow(){
-    this.x = this.x -1;
+
+      this.x = this.x -1;
+
 
   }
+
+}
+
+class Avatar{
+  constructor(x,y){
+    this.x=x;
+    this.y=y;
+  }
+
+  drawMe(){
+    fill("black")
+    stroke("black")
+    ellipse(this.x,this.y,20,20)
+    //print(this.y)
+  }
+
+  moveMe(){
+    if (keyIsDown(UP_ARROW)) { //if you hold the up arrow, move up by speed
+       this.y -= 10;
+    }
+
+    if (groundtest[0]<100) { // if you hold the down arrow, move down by speed
+        this.y += 3;
+    }
+
+	}
+
+
 
 }
